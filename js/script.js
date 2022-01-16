4522cc62b4e54451c655e7dd143be16f01e46726
Krzysztof Kral <christoph3r.kral@gmail.com>
2022-01-16 18:08:39 +0000

    finishing touches, adding active class to right article

--- js/script.js
+++ js/script.js
@@ -34,11 +34,12 @@ for(let activeLink of activeLinks){
   console.log(articleSelector)
 
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 
-  
 }
   
   const links = document.querySelectorAll('.titles a');
