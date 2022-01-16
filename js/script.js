988f1bb72e9994bcdb7731a29b5562f64f3bcd63
Krzysztof Kral <christoph3r.kral@gmail.com>
2022-01-16 17:43:02 +0000

    creating 'this' and adding 'href'

--- js/script.js
+++ js/script.js
@@ -8,33 +8,37 @@
 
 
 const titleClickHandler = function(event){
-    console.log('Link was clicked!');
+    event.preventDefault();
+    const clickedElement = this;  
+    
+/* [DONE] remove class 'active' from all article links  */
 
-  
-/* remove class 'active' from all article links  */
-
-const activeLinks = document.querySelectorAll('.posts .active');
+const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
 
-
-  /* remove class 'active' from all articles */
-
-  const activeArticles = document.querySelectorAll('.titles a.active');
-
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts .active');
+  
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector)
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
+
   /* add class 'active' to the correct article */
+
+  
 }
   
   const links = document.querySelectorAll('.titles a');
