9ea5982fa9951d81116ff2ccb1af0448fa486210
Krzysztof Kral <christoph3r.kral@gmail.com>
2022-01-16 19:27:10 +0000

    small touches

--- js/script.js
+++ js/script.js
@@ -13,11 +13,11 @@ const titleClickHandler = function(event){
     
 /* [DONE] remove class 'active' from all article links  */
 
-const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
   /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
